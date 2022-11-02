cbe1aa53032d1f6cc3d01a52171c8ed93720ee2f
Charlie <charlie.lidbury@icloud.com>
2022-11-02 15:35:31 +0000

    refactor: everything

--- plot.py
+++ plot.py
@@ -1,54 +1,60 @@
+from typing import Optional
 import matplotlib.pyplot as plt
 
 HEIGHT_OFFSET = 5
 WIDTH_OFFSET = 50
 
+
 class Decision:
 
-  def __init__(self, emitter: int, value: int):
-    self.emitter = emitter
-    self.value = value
+    def __init__(self, emitter: int, value: int):
+        self.emitter = emitter
+        self.value = value
+
 
 class TreeNode:
 
-  def __init__(self, label: Decision | int, depth: int):
-    self.left = None
-    self.right = None
-    self.label = label
-    self.depth = depth
+    def __init__(self, label: Decision | int, left=None, right=None):
+        self.label = label
+
+        self.left = left
+        self.right = right
+
+        self.depth = 1 + max(
+            left.depth if left else 0,
+            right.depth if right else 0
+        )
 
 
 def plot_tree_node(node: TreeNode, x: int):
 
-  y = node.depth * HEIGHT_OFFSET
+    y = node.depth * HEIGHT_OFFSET
 
-  # is a branch node
-  if isinstance(node.label, Decision):
-    label = 'E' + str(node.label.emitter) + ' < ' + str(node.label.value)
-    plt.text(x, y, label)
-    plot_tree_node(node.left, x - WIDTH_OFFSET)
-    plot_tree_node(node.right, x + WIDTH_OFFSET)
+    # is a branch node
+    if isinstance(node.label, Decision):
+        label = 'E' + str(node.label.emitter) + ' < ' + str(node.label.value)
+        plt.text(x, y, label)
+        plot_tree_node(node.left, x - WIDTH_OFFSET)
+        plot_tree_node(node.right, x + WIDTH_OFFSET)
 
-  else:
-    label = "Leaf: " + str(node.label)
-    plt.text(x, y, label)
-
-  
+    else:
+        label = "Leaf: " + str(node.label)
+        plt.text(x, y, label)
 
 
 def plot_decision_tree(tree: TreeNode, max_depth: int):
 
-  max_y = HEIGHT_OFFSET * max_depth
-  max_x = WIDTH_OFFSET * (max_depth ** 2)
+    max_y = HEIGHT_OFFSET * max_depth
+    max_x = WIDTH_OFFSET * (max_depth ** 2)
 
-  plot_tree_node(tree, 0)
+    plot_tree_node(tree, 0)
 
-  ax = plt.gca()
-  ax.set_xlim([-max_x, max_x])
-  ax.set_ylim([0, max_y])
+    ax = plt.gca()
+    ax.set_xlim([-max_x, max_x])
+    ax.set_ylim([0, max_y])
 
-  plt.xticks([])
-  plt.yticks([])
-  plt.plot()
-  plt.savefig('tree.png', format="png")
-  plt.show()
+    plt.xticks([])
+    plt.yticks([])
+    plt.plot()
+    plt.savefig('tree.png', format="png")
+    plt.show()
--- wifi.py
+++ wifi.py
@@ -7,106 +7,88 @@ LABEL_INDEX: int = 7
 
 
 def main():
-    clean_data = np.loadtxt("./WIFI_db/noisy_dataset.txt", dtype='float')
-    (tree, depth) = decision_tree_learning(clean_data, 0)
-    plot_decision_tree(tree, depth)
-    print(depth)
+    clean_data = np.loadtxt("./WIFI_db/clean_dataset.txt", dtype='int')
+    tree = decision_tree_learning(np.transpose(clean_data))
+    # plot_decision_tree(tree, tree.depth)
+    print(tree.depth)
     return 0
 
 
-def decision_tree_learning(data: np.ndarray, depth: int):
+def decision_tree_learning(dataT: np.ndarray):
+    labels = dataT[LABEL_INDEX]
 
-    label = np.transpose(data)[LABEL_INDEX]
+    # If all labels are the same, no more decisions to be made.
+    if np.all(labels[0] == labels):
+        return TreeNode(labels[0])
 
-    # if all samples in data have same label (col 7)
-    if np.all(label[0] == label):
-        return (TreeNode(label[0], depth), depth)
+    # Decision tree not perfect, 
+    (decision, leftData, rightData) = split(dataT)
 
-    (decision, leftData, rightData) = split(data)
+    left = decision_tree_learning(leftData)
+    right = decision_tree_learning(rightData)
 
-    node = TreeNode(decision, depth)
-
-    leftBranch, leftDepth = decision_tree_learning(leftData, depth+1)
-    rightBranch, rightDepth = decision_tree_learning(rightData, depth+1)
-
-    node.left = leftBranch
-    node.right = rightBranch
-
-    return (node, max(leftDepth, rightDepth))
+    return TreeNode(decision, left, right)
 
 
-def split(data: np.ndarray):
-
+def split(dataT: np.ndarray):
     # loop through every attribute, find split point and highest info gain
     # per attribute
-
-    bestIG = 0
-    splitVal = 0
-    bestAttr = -1
-    bestLeftData = bestRightData = None
-
-    dataT = np.transpose(data)
+    bestIg = 0
+    bestSplit = None
 
     for attr in range(LABEL_INDEX):
+        (ig, split) = attribute_split(dataT, attr)
 
-        (ig, decision, leftData, rightData) = attribute_split(dataT, attr)
+        if ig > bestIg:
+            bestIg = ig
+            bestSplit = split
 
-        if ig > bestIG:
-            bestIG = ig
-            bestDecision = decision
-            bestLeftData = leftData
-            bestRightData = rightData
-
-    return (bestDecision, np.transpose(bestLeftData), np.transpose(bestRightData))
+    return bestSplit
 
 
 # takes in 2 row array (row 0: attribute vals, row 1: labels)
 def attribute_split(dataT: np.ndarray, attr: int):
-
     # (highestIG, midpoint, leftData, rightData)
     # res = (0, 0, [[]], [[]])
-    res = None
-    highestIG = 0
+    bestIg = 0
+    bestSplit = None
 
     # sort attribute by value (first row)
     sortedDataT = dataT[:, dataT[attr].argsort()]
 
-    noOfExamples = np.shape(dataT)[1]
+    lenData = np.shape(dataT)[1]
 
-    for i in range(noOfExamples - 1):
-
+    for splitIdx in range(lenData - 1):
         # sliding window of size 2]
-        valPair = sortedDataT[attr][i: i + 2]
-
-        midpoint = (valPair[0] + valPair[1]) / 2
+        window = sortedDataT[attr][splitIdx: splitIdx + 2]
+        midpoint = (window[0] + window[1]) / 2
 
         # slice sorted 2d array into two parts, seperated by i
-        leftData = sortedDataT[:, :i+1]
-        rightData = sortedDataT[:, i+1:]
+        leftData = sortedDataT[:, :splitIdx + 1]
+        rightData = sortedDataT[:, splitIdx + 1:]
 
-        ig = infoGain(
-            sortedDataT[LABEL_INDEX], leftData[LABEL_INDEX], rightData[LABEL_INDEX])
+        ig = infoGain(sortedDataT[LABEL_INDEX], splitIdx)
 
         # update result var if new IG is higher
-        if ig > highestIG:
-            highestIG = ig
-            res = (highestIG, midpoint, leftData, rightData)
+        if ig > bestIg:
+            bestIg = ig
+            bestSplit = (Decision(attr, midpoint), leftData, rightData)
 
-    return res
+    return (bestIg, bestSplit)
 
 
-def infoGain(dataLabels, leftLabels, rightLabels):
-    return entropy(dataLabels) - remainder(leftLabels, rightLabels)
+def infoGain(labels, splitIdx):
+    return entropy(labels) - remainder(labels[: splitIdx + 1], labels[splitIdx + 1:])
 
 
 def remainder(leftLabels: np.array, rightLabels: np.array):
-
     leftTotal = np.shape(leftLabels)[0]
     rightTotal = np.shape(rightLabels)[0]
-    combinedTotal = leftTotal + rightTotal
 
-    leftRem = (leftTotal / combinedTotal) * entropy(leftLabels)
-    rightRem = (rightTotal / combinedTotal) * entropy(rightLabels)
+    total = leftTotal + rightTotal
+
+    leftRem = (leftTotal / total) * entropy(leftLabels)
+    rightRem = (rightTotal / total) * entropy(rightLabels)
 
     return leftRem + rightRem
 
